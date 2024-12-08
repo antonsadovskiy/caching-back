2a9504502702f7d4723f0b7bbe784cf316c95c3d
antonsadovskiy <antonsadovskiy6@gmail.com>
2024-12-08 20:19:50 +0300

    feat

--- api/index.js
+++ api/index.js
@@ -1,9 +1,25 @@
-
+const { createServer } = require("http");
+const { parse } = require("url");
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => {
+    res.json({ message: "Serverless API working!" });
+});
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.get("/users", (req, res) => {
+    res.json({ users: ["User1", "User2", "User3"] });
+});
 
-module.exports = app;
+// Экспорт функции, как требуется Vercel
+module.exports = (req, res) => {
+    const server = createServer((req, res) => {
+        const parsedUrl = parse(req.url, true);
+        app(req, res, parsedUrl);
+    });
+    server.emit("request", req, res);
+};
