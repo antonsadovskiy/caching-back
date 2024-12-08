68fde73690fc5ac7166e80e850e10d5f71c03e87
antonsadovskiy <antonsadovskiy6@gmail.com>
2024-12-08 19:37:34 +0300

    feat

---
+++ api/index.ts
@@ -0,0 +1,10 @@
+import type {Request, Response} from "express";
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req: Request, res: Response) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
